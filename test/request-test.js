28804374e4a66320e43fbac30acacbc355b88e6a
Alex Mathews <alexjmathews@gmail.com>
2017-06-08 09:22:37 -0400

    linter on test

--- test/request-test.js
+++ test/request-test.js
@@ -1,35 +1,36 @@
-var Request = require('../lib/request'),
-    should = require('should');
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "should" }]*/
 
-describe('Request unit tests', function () {
-  describe("matching", function () {
+const Request = require('../lib/request');
+const should = require('should');
 
-    it('should work with defined headers in the incoming request', function () {
-      var request = new Request(new Object(), {
-        method: 'GET',
-        url: '/lowercasetest',
-        headers: { 'foo-type': 'artischocke' }
-      });
+describe('Request unit tests', () => {
+	describe('matching', () => {
+		it('should work with defined headers in the incoming request', () => {
+			const request = new Request({}, {
+				method: 'GET',
+				url: '/lowercasetest',
+				headers: { 'foo-type': 'artischocke' }
+			});
 
-      request.isMatch({
-        method: 'GET',
-        url: '/lowercasetest',
-        headers: { 'foo-type': 'artischocke' }
-      }).should.equal(true);
-    });
+			request.isMatch({
+				method: 'GET',
+				url: '/lowercasetest',
+				headers: { 'foo-type': 'artischocke' }
+			}).should.equal(true);
+		});
 
-    it('should work with defined headers in the incoming request', function () {
-      var request = new Request(new Object(), {
-        method: 'GET',
-        url: '/lowercasetest',
-        headers: { 'foo-type': 'artischocke' }
-      });
+		it('should work with defined headers in the incoming request', () => {
+			const request = new Request({}, {
+				method: 'GET',
+				url: '/lowercasetest',
+				headers: { 'foo-type': 'artischocke' }
+			});
 
-      request.isMatch({
-        method: 'GET',
-        url: '/lowercasetest',
-        headers: {}
-      }).should.equal(false);
-    });
-  });
+			request.isMatch({
+				method: 'GET',
+				url: '/lowercasetest',
+				headers: {}
+			}).should.equal(false);
+		});
+	});
 });
--- test/simple-test.js
+++ test/simple-test.js
@@ -1,288 +1,286 @@
-var http = require('http'),
-    should = require('should'),
-    shouldHttp = require('should-http'),
-    request = require('request'),
-    hock = require('../');
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "should" }]*/
 
-var PORT = 5678;
+const http = require('http');
+const should = require('should');
+const shouldHttp = require('should-http');
+const request = require('request');
+const hock = require('../');
 
-describe('Hock HTTP Tests', function() {
+const PORT = 5678;
 
-  var hockInstance;
-  var httpServer;
+describe('Hock HTTP Tests', () => {
+	let hockInstance;
+	let httpServer;
 
-  describe("with available ports", function() {
-    before(function(done) {
-      hockInstance = hock.createHock();
-      httpServer = http.createServer(hockInstance.handler).listen(PORT, function(err) {
-        should.not.exist(err);
-        should.exist(hockInstance);
+	describe('with available ports', () => {
+		before((done) => {
+			hockInstance = hock.createHock();
+			httpServer = http.createServer(hockInstance.handler).listen(PORT, (err) => {
+				should.not.exist(err);
+				should.exist(hockInstance);
 
-        done();
-      });
-    });
+				done();
+			});
+		});
 
-    it('should correctly respond to an HTTP GET request', function(done) {
-      hockInstance
+		it('should correctly respond to an HTTP GET request', (done) => {
+			hockInstance
         .get('/url')
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      request('http://localhost:' + PORT + '/url', function(err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(200);
-        JSON.parse(body).should.eql({ 'hock': 'ok' });
-        done();
+			request(`http://localhost:${PORT}/url`, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(200);
+				JSON.parse(body).should.eql({ hock: 'ok' });
+				done();
+			});
+		});
 
-      });
-    });
+		it('should correctly respond to an HTTP POST request', (done) => {
+			hockInstance
+        .post('/post', { hock: 'post' })
+        .reply(201, { hock: 'created' });
 
-    it('should correctly respond to an HTTP POST request', function (done) {
-      hockInstance
-        .post('/post', { 'hock': 'post' })
-        .reply(201, { 'hock': 'created' });
+			request({
+				uri: `http://localhost:${PORT}/post`,
+				method: 'POST',
+				json: {
+					hock: 'post'
+				}
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(201);
+				body.should.eql({ hock: 'created' });
+				done();
+			});
+		});
 
-      request({
-        uri: 'http://localhost:' + PORT + '/post',
-        method: 'POST',
-        json: {
-          'hock': 'post'
-        }
-      }, function (err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(201);
-        body.should.eql({ 'hock': 'created' });
-        done();
-      });
-    });
+		it('should correctly respond to an HTTP PUT request', (done) => {
+			hockInstance
+        .put('/put', { hock: 'put' })
+        .reply(204, { hock: 'updated' });
 
-    it('should correctly respond to an HTTP PUT request', function (done) {
-      hockInstance
-        .put('/put', { 'hock': 'put' })
-        .reply(204, { 'hock': 'updated' });
+			request({
+				uri: `http://localhost:${PORT}/put`,
+				method: 'PUT',
+				json: {
+					hock: 'put'
+				}
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(204);
+				should.not.exist(body);
+				done();
+			});
+		});
 
-      request({
-        uri: 'http://localhost:' + PORT + '/put',
-        method: 'PUT',
-        json: {
-          'hock': 'put'
-        }
-      }, function (err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(204);
-        should.not.exist(body);
-        done();
-      });
-    });
+		it('should correctly respond to an HTTP PATCH request', (done) => {
+			hockInstance
+        .patch('/patch', { hock: 'patch' })
+        .reply(204, { hock: 'updated' });
 
-    it('should correctly respond to an HTTP PATCH request', function (done) {
-      hockInstance
-        .patch('/patch', { 'hock': 'patch' })
-        .reply(204, { 'hock': 'updated' });
+			request({
+				uri: `http://localhost:${PORT}/patch`,
+				method: 'PATCH',
+				json: {
+					hock: 'patch'
+				}
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(204);
+				should.not.exist(body);
+				done();
+			});
+		});
 
-      request({
-        uri: 'http://localhost:' + PORT + '/patch',
-        method: 'PATCH',
-        json: {
-          'hock': 'patch'
-        }
-      }, function (err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(204);
-        should.not.exist(body);
-        done();
-      });
-    });
-
-    it('should correctly respond to an HTTP DELETE request', function (done) {
-      hockInstance
+		it('should correctly respond to an HTTP DELETE request', (done) => {
+			hockInstance
         .delete('/delete')
-        .reply(202, { 'hock': 'deleted' });
+        .reply(202, { hock: 'deleted' });
 
-      request({
-        uri: 'http://localhost:' + PORT + '/delete',
-        method: 'DELETE'
-      }, function (err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(202);
-        should.exist(body);
-        JSON.parse(body).should.eql({ 'hock': 'deleted' });
-        done();
-      });
-    });
+			request({
+				uri: `http://localhost:${PORT}/delete`,
+				method: 'DELETE'
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(202);
+				should.exist(body);
+				JSON.parse(body).should.eql({ hock: 'deleted' });
+				done();
+			});
+		});
 
-    it('should correctly respond to an HTTP HEAD request', function (done) {
-      hockInstance
+		it('should correctly respond to an HTTP HEAD request', (done) => {
+			hockInstance
         .head('/head')
         .reply(200, '', { 'Content-Type': 'plain/text' });
 
-      request({
-        uri: 'http://localhost:' + PORT + '/head',
-        method: 'HEAD'
-      }, function (err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(200);
-        should.exist(body);
-        body.should.equal('');
-        res.should.have.header('content-type', 'plain/text');
-        done();
-      });
-    });
+			request({
+				uri: `http://localhost:${PORT}/head`,
+				method: 'HEAD'
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(200);
+				should.exist(body);
+				body.should.equal('');
+				res.should.have.header('content-type', 'plain/text');
+				done();
+			});
+		});
 
-    it('should correctly respond to an HTTP COPY request', function(done) {
-      hockInstance
+		it('should correctly respond to an HTTP COPY request', (done) => {
+			hockInstance
         .copy('/copysrc')
         .reply(204);
 
-      request({
-        uri: 'http://localhost:' + PORT + '/copysrc',
-        method: 'COPY'
-      }, function(err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(204);
-        body.should.equal('');
-        done();
-      });
-    });
+			request({
+				uri: `http://localhost:${PORT}/copysrc`,
+				method: 'COPY'
+			}, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(204);
+				body.should.equal('');
+				done();
+			});
+		});
 
-    it('unmatched requests should throw', function () {
-      hockInstance
+		it('unmatched requests should throw', () => {
+			hockInstance
         .head('/head')
         .reply(200, '', { 'Content-Type': 'plain/text' });
 
-      (function() {
-        hockInstance.done();
-      }).should.throw();
-    });
+			(function () {
+				hockInstance.done();
+			}).should.throw();
+		});
 
-    it('unmatched requests should call done callback with err', function (done) {
-      hockInstance
+		it('unmatched requests should call done callback with err', (done) => {
+			hockInstance
         .head('/head')
         .reply(200, '', { 'Content-Type': 'plain/text' })
-        .done(function(err) {
-          should.exist(err);
-          done();
-        });
-    });
+        .done((err) => {
+	should.exist(err);
+	done();
+});
+		});
 
-    after(function (done) {
-      httpServer.close(done);
-    });
-  });
+		after((done) => {
+			httpServer.close(done);
+		});
+	});
 
-  describe("dynamic path replacing / filtering", function() {
-    before(function(done) {
-      hockInstance = hock.createHock();
-      httpServer = http.createServer(hockInstance.handler).listen(PORT, function(err) {
-        should.not.exist(err);
-        should.exist(hockInstance);
+	describe('dynamic path replacing / filtering', () => {
+		before((done) => {
+			hockInstance = hock.createHock();
+			httpServer = http.createServer(hockInstance.handler).listen(PORT, (err) => {
+				should.not.exist(err);
+				should.exist(hockInstance);
 
-        done();
-      });
-    });
+				done();
+			});
+		});
 
-    it('should correctly use regex', function(done) {
-      hockInstance
+		it('should correctly use regex', (done) => {
+			hockInstance
         .filteringPathRegEx(/password=[^&]*/g, 'password=XXX')
         .get('/url?password=XXX')
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      request('http://localhost:' + PORT + '/url?password=artischocko', function(err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(200);
-        JSON.parse(body).should.eql({ 'hock': 'ok' });
-        done();
+			request(`http://localhost:${PORT}/url?password=artischocko`, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(200);
+				JSON.parse(body).should.eql({ hock: 'ok' });
+				done();
+			});
+		});
 
-      });
-    });
-
-    it('should correctly use functions', function(done) {
-      hockInstance
-        .filteringPath(function (p) {
-          p.should.equal('/url?password=artischocko');
-          return '/url?password=XXX';
-        })
+		it('should correctly use functions', (done) => {
+			hockInstance
+        .filteringPath((p) => {
+	p.should.equal('/url?password=artischocko');
+	return '/url?password=XXX';
+})
         .get('/url?password=XXX')
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      request('http://localhost:' + PORT + '/url?password=artischocko', function(err, res, body) {
-        should.not.exist(err);
-        should.exist(res);
-        res.statusCode.should.equal(200);
-        JSON.parse(body).should.eql({ 'hock': 'ok' });
-        done();
+			request(`http://localhost:${PORT}/url?password=artischocko`, (err, res, body) => {
+				should.not.exist(err);
+				should.exist(res);
+				res.statusCode.should.equal(200);
+				JSON.parse(body).should.eql({ hock: 'ok' });
+				done();
+			});
+		});
 
-      });
-    });
+		after((done) => {
+			httpServer.close(done);
+		});
+	});
 
-    after(function(done) {
-      httpServer.close(done);
-    });
-  });
+	describe('test if route exists', () => {
+		before((done) => {
+			hockInstance = hock.createHock();
+			httpServer = http.createServer(hockInstance.handler).listen(PORT, (err) => {
+				should.not.exist(err);
+				should.exist(hockInstance);
 
-  describe("test if route exists", function() {
-    before(function(done) {
-      hockInstance = hock.createHock();
-      httpServer = http.createServer(hockInstance.handler).listen(PORT, function (err) {
-        should.not.exist(err);
-        should.exist(hockInstance);
+				done();
+			});
+		});
 
-        done();
-      });
-    });
-
-    it('should allow testing for url', function(done) {
-      hockInstance
+		it('should allow testing for url', (done) => {
+			hockInstance
         .get('/url?password=foo')
-        .reply(200, { 'hock': 'ok' })
+        .reply(200, { hock: 'ok' })
         .get('/arti')
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      hockInstance.hasRoute('GET', '/url?password=foo').should.equal(true);
-      hockInstance.hasRoute('GET', '/arti').should.equal(true);
-      hockInstance.hasRoute('GET', '/notexist').should.equal(false);
-      done();
-    });
+			hockInstance.hasRoute('GET', '/url?password=foo').should.equal(true);
+			hockInstance.hasRoute('GET', '/arti').should.equal(true);
+			hockInstance.hasRoute('GET', '/notexist').should.equal(false);
+			done();
+		});
 
-    it('matches the header', function(done) {
-      hockInstance
+		it('matches the header', (done) => {
+			hockInstance
         .get('/url?password=foo')
-        .reply(200, { 'hock': 'ok' })
+        .reply(200, { hock: 'ok' })
         .get('/artischocko', { 'foo-type': 'artischocke' })
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      hockInstance
+			hockInstance
         .hasRoute('GET', '/bla?password=foo', null, { 'content-type': 'plain/text' })
         .should.equal(false);
-      hockInstance
+			hockInstance
         .hasRoute('GET', '/artischocko', null, { 'foo-type': 'artischocke' })
         .should.equal(true);
 
-      done();
-    });
+			done();
+		});
 
-    it('matches the body', function(done) {
-      hockInstance
+		it('matches the body', (done) => {
+			hockInstance
         .get('/url?password=foo')
-        .reply(200, { 'hock': 'ok' })
+        .reply(200, { hock: 'ok' })
         .post('/artischocko', 'enteente')
-        .reply(200, { 'hock': 'ok' });
+        .reply(200, { hock: 'ok' });
 
-      hockInstance.hasRoute('GET', '/bla?password=foo', 'testing').should.equal(false);
-      hockInstance.hasRoute('POST', '/artischocko', 'enteente').should.equal(true);
+			hockInstance.hasRoute('GET', '/bla?password=foo', 'testing').should.equal(false);
+			hockInstance.hasRoute('POST', '/artischocko', 'enteente').should.equal(true);
 
-      done();
-    });
+			done();
+		});
 
-    after(function(done) {
-      httpServer.close(done);
-    });
-  });
+		after((done) => {
+			httpServer.close(done);
+		});
+	});
 });
